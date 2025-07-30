ce41978fb721a63adaeb97bcb80f9066d31107ab
clement Roland <clementroland52@gmail.com>
2025-07-30 15:13:08 -0400

    Ajout de la section "Nos Flottes" dans le composant Home avec des informations sur les véhicules, y compris des images et des détails sur la transmission et la capacité. Mise à jour des styles SCSS pour la nouvelle section et suppression de deux images inutilisées.

---
+++ src/app/reservation-bar/reservation-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-bar',
+  imports: [],
+  templateUrl: './reservation-bar.component.html',
+  styleUrl: './reservation-bar.component.scss'
+})
+export class ReservationBarComponent {
+
+}
