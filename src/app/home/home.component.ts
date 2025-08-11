3622b62ddd9e48c316668302f52ebfb0eb3561e0
clement Roland <clementroland52@gmail.com>
2025-08-11 15:50:40 -0400

    Ajout du composant Footer dans app.component et mise à jour des imports. Ajustement de la largeur des champs d'entrée dans le composant mobile-reservation-modal pour une meilleure réactivité. Modification de la règle de réactivité dans le composant reservation-bar pour s'adapter à des écrans plus larges.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,OnDestroy,AfterViewInit,ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { ReservationBarComponent } from '../reservation-bar/reservation-bar.component';
+
 @Component({
   selector: 'app-home',
   imports: [CommonModule,MatIconModule,ReservationBarComponent],
