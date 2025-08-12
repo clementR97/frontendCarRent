7dee57b413705d5706ca473c2f4bf06ea8f613e9
clement Roland <clementroland52@gmail.com>
2025-08-12 14:53:10 -0400

    Ajout d'un nouveau composant pour la gestion des réservations, incluant des fonctionnalités de sélection de date et d'heure. Mise à jour des styles SCSS pour améliorer l'apparence et la réactivité du composant. Ajustements des imports dans les composants concernés.

---
+++ src/app/connexion-user/connexion-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connexion-user',
+  imports: [],
+  templateUrl: './connexion-user.component.html',
+  styleUrl: './connexion-user.component.scss'
+})
+export class ConnexionUserComponent {
+
+}
